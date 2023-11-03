98224fc1253ca2da4254b078e9f3cba0ca5f3b7b
Dilbekhon <127374324+Dilbekhon@users.noreply.github.com>
2023-11-03 10:51:00 -0400

    Update task_1.h

--- src/task_1.h
+++ src/task_1.h
@@ -1,5 +1,26 @@
-/*
- * Author:
- * Date:
- * Name:
- */
+#include <iostream>
+
+enum Day { Sunday, Monday, Tuesday, Wednesday, Thursday, Friday, Saturday };
+
+Day calculateFutureDay(Day today, int numDays) {
+    int futureDay = (today + numDays) % 7;
+    return static_cast<Day>(futureDay);
+}
+
+std::string getDayName(Day day) {
+    std::string dayNames[] = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
+    return dayNames[day];
+}
+
+int main() {
+    Day today = Thursday;
+    int numDays = 98;
+
+    Day futureDay = calculateFutureDay(today, numDays);
+    std::string futureDayName = getDayName(futureDay);
+
+    std::cout << "Today is " << getDayName(today) << std::endl;
+    std::cout << "After " << numDays << " days, it will be " << futureDayName << std::endl;
+
+    return 0;
+}
