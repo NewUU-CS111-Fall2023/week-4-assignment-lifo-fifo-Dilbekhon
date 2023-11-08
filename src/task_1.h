aa0328f55f90948078d59cf5b30bf118139181b3
Dilbekhon <127374324+Dilbekhon@users.noreply.github.com>
2023-11-08 10:57:33 -0500

    Update task_1.h

--- src/task_1.h
+++ src/task_1.h
@@ -1,26 +1,63 @@
 #include <iostream>
 
-enum Day { Sunday, Monday, Tuesday, Wednesday, Thursday, Friday, Saturday };
+struct Node {
+    int data;
+    Node* next;
+};
 
-Day calculateFutureDay(Day today, int numDays) {
-    int futureDay = (today + numDays) % 7;
-    return static_cast<Day>(futureDay);
-}
+class Stack {
+private:
+    Node* top; // Pointer to the top of the stack
 
-std::string getDayName(Day day) {
-    std::string dayNames[] = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
-    return dayNames[day];
-}
+public:
+    Stack() {
+        top = nullptr; // Initialize the stack as empty
+    }
+
+    // Push an element onto the stack
+    void push(int value) {
+        Node* newNode = new Node;
+        newNode->data = value;
+        newNode->next = top;
+        top = newNode;
+    }
+
+    int pop() {
+        if (isEmpty()) {
+            std::cerr << "Stack is empty. Cannot pop." << std::endl;
+            return -1; // Return a default value
+        }
+        int value = top->data;
+        Node* temp = top;
+        top = top->next;
+        delete temp;
+        return value;
+    }
+    int peek() {
+        if (isEmpty()) {
+            std::cerr << "Stack is empty. Cannot peek." << std::endl;
+            return -1; // Return a default value
+        }
+        return top->data;
+    }
+
+    bool isEmpty() {
+        return top == nullptr;
+    }
+};
 
 int main() {
-    Day today = Thursday;
-    int numDays = 98;
+    Stack stack;
 
-    Day futureDay = calculateFutureDay(today, numDays);
-    std::string futureDayName = getDayName(futureDay);
+    stack.push(1);
+    stack.push(2);
+    stack.push(3);
 
-    std::cout << "Today is " << getDayName(today) << std::endl;
-    std::cout << "After " << numDays << " days, it will be " << futureDayName << std::endl;
+    std::cout << "Top element: " << stack.peek() << std::endl;
+
+    while (!stack.isEmpty()) {
+        std::cout << "Popped: " << stack.pop() << std::endl;
+    }
 
     return 0;
 }
